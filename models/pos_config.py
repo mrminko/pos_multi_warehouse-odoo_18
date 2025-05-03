d7ca6615dbf2d94cbaae49adc59f8d35cf2d8582
Min Ko Oo <145268492+mrminko@users.noreply.github.com>
2025-05-03 11:17:32 +0900

    Update pos_config.py

--- models/pos_config.py
+++ models/pos_config.py
@@ -6,7 +6,7 @@ class PosConfig(models.Model):
     def switch_pos_picking_type(self, operation_type_id):
         self.ensure_one()
         if self.picking_type_id == operation_type_id:
-            return self.picking_type_id.name
+            return {"from_location": {"id": self.picking_type_id.default_location_src_id.id}, "name":self.picking_type_id.name}
         operation = self.env['stock.picking.type'].search([('id', '=', operation_type_id)])
         if not operation:
             return False
